ce6229101b6638d3c616c1f9ad722ffaec9587cf
samuripper <samu@linuxasylum.net>
2012-04-03 11:58:51 +0200

    aggiunta della libreria base che gestisce parametri di configurazione e html per login e logout

---
+++ include/basic.php
@@ -0,0 +1,52 @@
+<?
+
+require_once "HTML/Template/IT.php";
+#su debian e UBUNTU restano cosi'
+define('IN_PHPBB', true);
+define('PHPBB_ROOT_PATH', '/usr/share/phpbb3/www/');
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+
+
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : '/usr/share/phpbb3/www/';
+include($phpbb_root_path . 'common.php');
+
+
+## URL di visibilita' sul web del forum phpBB3
+## forse si puo' togliere
+$phpbb_url = "/phpbb/";
+
+## directory di base dell'applicazione
+## forse si puo' togliere
+$root_path = '/var/www/sw/cnuovo/';
+
+#parametri di input:
+# root_path = path per arrivare alla directory base dei template
+# redir = ridirezione della pagina dopo il login
+# phpbb_url = url web a phpbb
+# output
+# ritorna il codice html del login 
+
+function miologin($root_path, $redir , $phpbb_url){
+        $tpl = new HTML_Template_IT($root_path . "/templates/default/");
+        $tpl->loadTemplatefile("login.tpl.htm", true, true);
+        $tpl->setVariable("PHPBB_URL", "$phpbb_url") ;
+        $tpl->setVariable("REDIRECT", $redir);
+        $tpl->parse("login");
+        $xxx = $tpl->get();
+        return $xxx;
+}
+
+function eddairegistrati(){
+
+}
+
+function miologout($user, $firstpage, $phpbb_url){
+        $user->session_kill();
+        $user->session_begin();
+        echo "<form method='post' action='" . $firstpage . "'>\n";
+        echo "<input type='submit' value='Log out' name='logout' />\n";
+        echo "</form>\n";
+}
+
+?>
+
