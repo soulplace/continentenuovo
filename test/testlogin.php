e64e0f76f243f98303f0faed3d4b7623c052b661
samuripper <samu@linuxasylum.net>
2012-05-04 11:29:00 +0200

    mancavi tu

---
+++ test/testlogin.php
@@ -0,0 +1,28 @@
+<?
+
+
+## $path = '/var/www/sw/cnuovo';
+## set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+## include('include/basic.php');
+echo "saaaaaaaaaaaaaaaaaaaaaaaaa";
+
+echo "<br>saaaaaaaaaaaaaaaaaaaaaaaaa";
+include('/var/www/sw/cnuovo/include/basic.php');
+
+$user->session_begin();
+$auth->acl($user->data);
+$user->setup();
+
+if ($user->data['user_id'] == ANONYMOUS) {
+	echo "anonimo";
+        $kk = miologin($root_path,$_SERVER["REQUEST_URI"], $phpbb_url);
+        print $kk;
+	
+} else {
+	echo "loggato";
+        $ww = miologout($root_path,$_SERVER["REQUEST_URI"], $web_url . "/logout.php");
+        print $ww;
+}
+
+?>
+
