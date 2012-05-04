e3978c8fa9491926d66f3de817991ac4716f9666
Fabio <duma.fabio@gmail.com>
2012-05-04 19:22:02 +0200

    aggiunta editor e uploader (da finire)

--- include/band_registration.php
+++ include/band_registration.php
@@ -1,13 +1,19 @@
+<link rel="stylesheet" type="text/css" href="/continentenuovo/js/uploadify/uploadify.css" />
+<link href="/continentenuovo/js/ckeditor/ckeditor.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="/continentenuovo/js/uploadify/jquery.uploadify-3.1.min.js"></script>
+<script type="text/javascript" src="/continentenuovo/js/ckeditor/ckeditor.js"></script>
 <?
 include_once($root_path . "include/login.php");
 /*echo "<pre>";
 print_r($user->data);
 echo "</pre>";*/
-$id_user = $user->data["user_id"];
-require_once "HTML/Template/IT.php";
-$tpl = new HTML_Template_IT("./templates/default/");
-$tpl->loadTemplatefile("band_registration.tpl.htm", true, true);
-$tpl->setVariable("ID_USER", $id_user) ;
-$tpl->parse("registration");
-$tpl->show();
+if($loggato){
+	$id_user = $user->data["user_id"];
+	require_once "HTML/Template/IT.php";
+	$tpl = new HTML_Template_IT("./templates/default/");
+	$tpl->loadTemplatefile("band_registration.tpl.htm", true, true);
+	$tpl->setVariable("ID_USER", $id_user) ;
+	$tpl->parse("registration");
+	$tpl->show();
+}
 ?>
